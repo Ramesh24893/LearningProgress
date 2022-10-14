bd3a0248af07ae15b459e504f9c1ed1c21a4d2b4
Ramesh24893 <111510788+Ramesh24893@users.noreply.github.com>
2022-10-14 13:41:22 +0530

    Add files via upload

---
+++ SOLID/ISP/LSP1/A_Theory.java
@@ -0,0 +1,9 @@
+//----------------------------INTERFACE SEGREGATION PRINCIPLE-------------------
+//We should not be force the users to depend upon interfaces that they do not use
+//This principle helps how to create interface instead of creating a interface with lot of functionalities
+//Violation of ISP leads to violation of Single Responsibility principle
+//maintenance becomes harder
+//Client developers are confused by the methods they don’t need
+//Interface with lot of functionalities cause lot of problems and it is called Fat interface
+//Implementing classes  don't need to behave same like superclasses, just enough to follow syntactically
+
