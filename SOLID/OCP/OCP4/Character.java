7a927583fcde00661d7966dbabafc499eae89720
Ramesh24893 <111510788+Ramesh24893@users.noreply.github.com>
2022-10-14 11:38:26 +0530

    Add files via upload

---
+++ SOLID/OCP/OCP4/Character.java
@@ -0,0 +1,29 @@
+// IN THIS CODE I CAN ADD NEW CHARACTER BY CREATING NEW CHARACTER CLASS AND IMPLEMENTING THE INTERFACE CHARACTER
+
+package OCP4;
+public interface Character {
+    public void  attack();
+    public void jump();
+}
+class Mario implements Character{
+
+    @Override
+    public void attack() {
+
+    }
+    @Override
+    public void jump() {
+    }
+}
+class Loki implements  Character {
+
+    @Override
+    public void attack() {
+
+    }
+    @Override
+    public void jump() {
+
+    }
+
+}
