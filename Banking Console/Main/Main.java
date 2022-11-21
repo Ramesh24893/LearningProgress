ad35f22d2d694b6af66dd65f61f5d9ee39bd567e
Ramesh24893 <111510788+Ramesh24893@users.noreply.github.com>
2022-11-21 18:53:58 +0530

    Add files via upload

---
+++ Banking Console/Main/Main.java
@@ -0,0 +1,16 @@
+package Main;
+
+
+import Controller.MainMenuController;
+import Data.InitialData;
+
+
+public class Main {
+    public static void main(String[] args) {
+        InitialData.initialData();
+        MainMenuController mainMenuController =new MainMenuController();
+        mainMenuController.menuController();
+    }
+}
+
+
