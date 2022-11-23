d64f8b6671a65cacf5ded13e88e6012c00dce61e
Ramesh24893 <111510788+Ramesh24893@users.noreply.github.com>
2022-11-23 12:18:49 +0530

    Delete Banking Console/Controller directory

--- Banking Console/Controller/BankTransactionDataGenerator.java
+++
@@ -1,9 +0,0 @@
-package Controller;
-
-public interface BankTransactionDataGenerator {
-    String generateWithDrawTransactionData(long amount, long accountBalance, long taxAmount);
-    String generateDepositTransactionData(long amount,long accountBalance,long taxAmount);
-    String generateSenderTransactionData(long amount,long receiverAccountNum,long accountBalance,long taxAmount);
-     String generateReceiverTransactionData(long amount,long senderAccountNum,long accountBalance);
-
-}
